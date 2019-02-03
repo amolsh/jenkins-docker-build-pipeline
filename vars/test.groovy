55338c73c98b4cc384568a5734ce321e50114ad2
Amol Shende <ashende@equalexperts.com>
2019-02-03 20:55:25 +0530

    added test functions in in vars

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,7 @@
+import com.amc.jenkinscommons.*
+// instantiate
+pl = new utilityFuncs()
+
 def call(Map args = [:]) {
     caughtError = 0
     ENV = args.get('environment') ?: 'dev'
@@ -6,16 +10,17 @@ def call(Map args = [:]) {
     try {
         node (NODE) {
             stage('Init') {
-                echo "Checking out code..."
+                echo "Checking out code...${ENV}"
+                pl.build()
 
             }
-            withCredentials([usernamePassword(credentialsId: 'nexus-service', passwordVariable: 'nexus_password', usernameVariable: 'nexus_username'), usernameColonPassword(credentialsId: 'nexus-service', variable: 'nexus_creds')]) {
-                stage('Build') {
-                    // BUILD COMMANDS GET RUN HERE
-                    cmd = sh(script: "python3 -u ./jenkins/c_build.py '"+MANIFEST_JSON+"'", returnStdout: true)
-                    sh "export 'NEXUS_CREDS="+nexus_creds+"' 'NEXUS_PWD="+nexus_password+"' 'NEXUS_USR="+nexus_username+"'; "+cmd+""
-                }
-            }
+            // withCredentials([usernamePassword(credentialsId: 'nexus-service', passwordVariable: 'nexus_password', usernameVariable: 'nexus_username'), usernameColonPassword(credentialsId: 'nexus-service', variable: 'nexus_creds')]) {
+            //     stage('Build') {
+            //         // BUILD COMMANDS GET RUN HERE
+            //         cmd = sh(script: "python3 -u ./jenkins/c_build.py '"+MANIFEST_JSON+"'", returnStdout: true)
+            //         sh "export 'NEXUS_CREDS="+nexus_creds+"' 'NEXUS_PWD="+nexus_password+"' 'NEXUS_USR="+nexus_username+"'; "+cmd+""
+            //     }
+            // }
 
             // If we got here, it means the build was a success.
             currentBuild.result = "SUCCESS"
